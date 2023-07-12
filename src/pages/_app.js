79592d10f3b116bdf57624f78019e1215b202591
Aaron <aaronli5260@gmail.com>
2023-07-11 21:47:01 -0700

    fuck typescript

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+import styles from '../styles/css/index.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} style={styles} />
 }
