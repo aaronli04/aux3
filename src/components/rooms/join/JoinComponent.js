7371acff3e2e88e38b21544cb72a37793ab761ce
Aaron <aaronli5260@gmail.com>
2023-07-24 16:09:38 -0700

    basic searchbar

--- src/components/rooms/join/JoinComponent.js
+++ src/components/rooms/join/JoinComponent.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react"
 import useRoom from "@/hooks/useRoom"
 import LoadingComponent from "@/components/shared/LoadingComponent";
+import { getPCN } from "@/utils/classes";
+import { GoSearch } from 'react-icons/go';
+
+const className = 'join'
+const pcn = getPCN(className)
 
 export default function JoinComponent() {
     const { getAllRooms } = useRoom();
@@ -21,6 +26,28 @@ export default function JoinComponent() {
     }
 
     return (
-        <div>here are some rooms to join...</div>
+        <div className={className}>
+            <div className={pcn('__liner')}>
+                <div className={pcn('__title')}>
+                    active rooms
+                </div>
+                <div className={pcn('__search-section')}>
+                    <div className={pcn('__searchbar')}>
+                        <GoSearch className={pcn('__search-icon')} size={25} />
+                        <input className={pcn('__search-text')} placeholder='Search' />
+                    </div>
+                    <div className={pcn('__separator')} />
+                    <div className={pcn('__search-results')}>
+                        {rooms.map((room, index) =>
+                            <div className={pcn('__search-result')} key={index}>
+                                <div className={pcn('__search-result-title')}>
+                                    {room.name}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
