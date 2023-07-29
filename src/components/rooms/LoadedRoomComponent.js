8d1b907ca2777327f6444f3bd532d517cb4a5cef
Aaron <aaronli5260@gmail.com>
2023-07-28 20:59:53 -0700

    song card

--- src/components/rooms/LoadedRoomComponent.js
+++ src/components/rooms/LoadedRoomComponent.js
@@ -1,8 +1,48 @@
 import { getPCN } from "@/utils/classes"
 import Link from "next/link"
+import SongCard from "../shared/SongCard"
 
 const className = 'loaded-room-component'
 const pcn = getPCN(className)
+
+const songs = [
+    {
+        name: 'ball w/o you',
+        image: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+        artist: '21 Savage',
+        votes: 123
+    },
+    {
+        name: 'ball w/o you',
+        image: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+        artist: '21 Savage',
+        votes: 123
+    },
+    {
+        name: 'ball w/o you',
+        image: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+        artist: '21 Savage',
+        votes: 123
+    },
+    {
+        name: 'ball w/o you',
+        image: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+        artist: '21 Savage',
+        votes: 123
+    },
+    {
+        name: 'ball w/o you',
+        image: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+        artist: '21 Savage',
+        votes: 123
+    },
+    {
+        name: 'ball w/o you',
+        image: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+        artist: '21 Savage',
+        votes: 123
+    },
+]
 
 export default function LoadedRoomComponent({ ownerInfo, roomInfo }) {
     return (
@@ -19,15 +59,23 @@ export default function LoadedRoomComponent({ ownerInfo, roomInfo }) {
                         <div className={pcn('__subtitle')}>
                             now playing
                         </div>
+                        <SongCard song={songs[0]} />
                     </div>
                     <div className={pcn('__queue-section')}>
                         <div className={pcn('__subtitle')}>
                             queue
                         </div>
                         <div className={pcn('__queue')}>
-
+                            {songs.map((song, index) =>
+                                <SongCard key={index} song={song}/>
+                            )}
                         </div>
                     </div>
+                </div>
+                <div className={pcn('__button-section')}>
+                    <button className={pcn('__add-to-queue')}>
+                        add to queue
+                    </button>
                 </div>
             </div>
         </div>
---
+++ src/components/shared/SongCard.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { BiSolidUpvote, BiSolidDownvote } from "react-icons/bi";
+import { getPCN } from "@/utils/classes";
+
+const className = 'song-card'
+const pcn = getPCN(className)
+
+export default function SongCard({ song }) {
+    return (
+        <div className={className}>
+            <div className={pcn('__liner')}>
+                <Image src={song.image} width={45} height={45} />
+                <div className={pcn('__song-info')}>
+                    <div className={pcn('__title')}>
+                        {song.name}
+                    </div>
+                    <div className={pcn('__artist')}>
+                        {song.artist}
+                    </div>
+                </div>
+                <div className={pcn('__vote-section')}>
+                    <button className={pcn('__downvote')}>
+                        <BiSolidDownvote size='30px'/>
+                    </button>
+                    <div className={pcn('__votes')}>
+                        {song.votes}
+                    </div>
+                    <button className={pcn('__upvote')}>
+                        <BiSolidUpvote size='30px'/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
